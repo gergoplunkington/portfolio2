073c0f76cdb166a02798742bb53b1e257768713c
gergoplunkington <gplikingtonkay@student.strode-college.ac.uk>
2023-08-26 00:15:06 +0100

    fef

--- app/page.tsx
+++ app/page.tsx
@@ -2,14 +2,13 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[url('/background-transformed.jpg')] bg-center bg-cover">
       
       <p>Hello world!</p>
       <Image src="/ed.jpg" alt="de.jpg" width={400} height={10}/>
       <h1>
         anything
       </h1>
-      <div className="bg-[url('public/background-transformed.jpeg')]"></div>
       <a href="#">bananana</a>
     </main>
   )
