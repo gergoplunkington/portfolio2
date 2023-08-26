8e4f7cc60f9e5b43e3df84f4dbab97ece832e347
gergoplunkington <gplikingtonkay@student.strode-college.ac.uk>
2023-08-26 21:06:12 +0100

    dam

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,35 @@
+"use client"
+import { ParallaxLayer } from '@react-spring/parallax'
+import { Parallax } from '@react-spring/parallax'
 import Image from 'next/image'
-
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[url('/background-transformed.jpeg')] bg-center bg-cover">
-      
+      <div>
+        <Parallax pages={2}>
+          <ParallaxLayer offset={0}>
+            <h2 className=" text-6xl textcenter font-bold ">welcome</h2>
+          </ParallaxLayer>
+          <ParallaxLayer offset={0.1} speed={0.5} factor={1}>
+            <h2>bungling ung</h2>
+          </ParallaxLayer>
+        </Parallax>
+      </div>
       <p>Hello world!</p>
       <Image src="/ed.jpg" alt="de.jpg" width={400} height={10}/>
-      <h1>
+      <div>
+        <Parallax pages={4}>
+          <ParallaxLayer offset={0} sticky={{start:0.2, end:2.4}}>
+            <h2> className="" mobinf dingdg</h2>
+          </ParallaxLayer>
+          <ParallaxLayer offset={0.1} speed={0.5} factor={1}>
+            <h2>bungling ung</h2>
+          </ParallaxLayer>
+        </Parallax>
+      </div>
+      <h2>
         anything
-      </h1>
+      </h2>
       <a href="#">bananana</a>
     </main>
   )
