7a53f0e36f431f768da7a8836ee8db7c93368e4c
gergoplunkington <gplikingtonkay@student.strode-college.ac.uk>
2023-08-26 21:06:00 +0100

    fuck me

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[url('/background-transformed.jpg')] bg-center bg-cover">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-[url('/background-transformed.jpeg')] bg-center bg-cover">
       
       <p>Hello world!</p>
       <Image src="/ed.jpg" alt="de.jpg" width={400} height={10}/>
